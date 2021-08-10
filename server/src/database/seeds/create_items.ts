68fb324182859d3e4ea025f6793e1f4aa5c0496f
DioenDJS <negociospacheti@gmail.com>
2021-08-10 02:21:08 -0300

    corrigindo erros no arquivo create_items do diretorio seeds que estava sem retorno e sem o await na function

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -1,7 +1,7 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex){
-    knex('item').insert([
+    return await knex('item').insert([
         {image: 'lampadas.svg' , title: 'Lâmpadas'},
         {image: 'baterias.svg', title: 'Pilhas e Baterias' },
         {image: 'papeis-papelao.svg', title: 'Papéis e papelão'},
